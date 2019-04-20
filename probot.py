8386173db8c6960ed7e06489f5e2ee334136c189
idkkk5 <44144811+idkkk5@users.noreply.github.com>
2019-04-20 23:46:10 +0300

    Update probot.py

--- probot.py
+++ probot.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import time
 import platform
 
-p=commands.Bot(command_prefix='P')
+p=commands.Bot(command_prefix='P',pm_help=True)
 start=time.time()
 
 async def playing():
@@ -26,6 +26,11 @@ async def on_ready():
   print('Bot is up!')
   p.loop.create_task(playing())
   
+@p.event
+async def on_member_join(member):
+    role = discord.utils.get(member.server.roles, id="Dab Member")
+    await p.add_roles(member, role)
+
 @p.command(pass_context=True)
 async def ping():
   await p.say('Why you ping me boi????')
