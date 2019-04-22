26c3665c65cc8ecb8f4b91c7cc38174cc1b48010
idkkk5 <44144811+idkkk5@users.noreply.github.com>
2019-04-22 14:05:19 +0300

    Update probot.py

--- probot.py
+++ probot.py
@@ -1,12 +1,10 @@
 import discord
 import asyncio
 from discord.ext import commands
-import json
 import os
 from time import strftime,localtime
 from datetime import datetime
 import time
-import platform
 import random
 
 p=commands.Bot(command_prefix='P',pm_help=True)
@@ -40,28 +38,42 @@ async def on_member_join(member):
 #@p.command(pass_context=True)
 #async def ping():
 #  await p.say('Why you ping me boi????')
-  
+
 p.remove_command('help')
 @p.command(pass_context=True)
 async def help(ctx):
     t=strftime('%d/%m/%Y %H:%M:%S',localtime())
-    embed=discord.Embed(color=0xFF00FF)
-    embed.add_field(name='Help',value='Shows this message')
-    embed.add_field(name='Pinfo [tag]',value='Shows info about a member.')
-    embed.add_field(name='Pmsg [#channel] [message]',value='Sends a message to a desired channel [OWNER ONLY]')
-    embed.set_footer(text=f'Sent at {t}.')
-    await p.send_message(ctx.message.author,embed=embed)
+    e=discord.Embed(color=0xFF00FF)
+    e.add_field(name='Help',value='Shows this message')
+    e.add_field(name='Pinfo [tag]',value='Shows info about a member.')
+    e.add_field(name='Pmsg [#channel] [message]',value='Sends a message to a desired channel [OWNER ONLY]')
+    e.add_field(name='Pavatar [tag]',value='Gives you a link to an avatar of a member.')
+    e.set_footer(text=f'Sent at {t}')
+    await p.send_message(ctx.message.author,embed=e)
 
 @p.command(pass_context=True)
 async def info(ctx,m:discord.Member):
-    await p.say(f'Info about {m.mention}:**\nName: {m.name}\nId: {m.id}\nCreated: {m.created_at}\nName on this server: {m.display_name}**')
-
+    t=strftime('%d/%m/%Y %H:%M:%S',localtime())
+    e=discord.Embed(title=f'Info for {m}',color=0x500000)
+    e.add_field(name='Name',value=m.name)
+    e.add_field(name='Created at',value=m.created_at)
+    e.add_field(name='Name on this server',value=m.display_name)
+    e.set_footer(text=f'Sent at {t}')
+    await p.say(embed=e)
+    
 @p.command(pass_context=True)
 async def msg(ctx,c:discord.Channel,*,m:str):
     if ctx.message.author.id==(dabs):
      await p.send_message(c,m)
      await p.say('Success ✓')
     else:
-     await p.say(f'No permission {ctx.message.author.mention}.')
+     await p.say(f'No permission, {ctx.message.author.mention}.')
 
-p.run(TOKEN
+@p.command(pass_context=True)
+async def avatar(ctx,u:discord.Member=None):
+    if u==p.user:
+        await p.say(f'{u.mention} uses a default avatar.')
+    else:
+        await p.say(f'Link: {u.avatar_url}')
+
+p.run(TOKEN)
